0276d65f0b5392d89f3495a904f304d17fccb909
Emmanuel Obi <emmanuel@5thcolumn.net>
2018-01-08 03:23:24 -0600

    make app viewable in browser

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("This is the app entry point")
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Relationship = require('./components/Relationship');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Relationship),
+    document.getElementById('mount')
+  );
+});
