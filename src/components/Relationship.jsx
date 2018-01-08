e53bec6912812144dd9c227ab20a7a6bc73aac33
Emmanuel Obi <emmanuel@5thcolumn.net>
2018-01-08 03:22:09 -0600

    add Parent (orchestration) component

---
+++ src/components/Relationship.jsx
@@ -0,0 +1,37 @@
+var React = require("react");
+var createReactClass = require('create-react-class');
+
+var Message = require('./Message');
+var Button = require('./Button');
+var HeartIcon = require('./HeartIcon');
+
+
+var imagePath = "src/images/";
+var Relationship = createReactClass(
+  {
+    getInitialState: function() {
+      return { isLoved: true };
+    },
+    toggleLove: function() {
+      this.setState({ isLoved: !this.state.isLoved });
+    },
+    render: function() {
+      var messageText = this.state.isLoved ? "Love me!" : "...or love me not."; 
+      var buttonText = this.state.isLoved ? "Unlove" : "Love";
+      return (
+        <table>
+          <tbody>
+            <tr>
+              <td><HeartIcon imagePath={ imagePath } isLoved={ this.state.isLoved }/></td>
+              <td style={ {width:"150px"} }><Message text={ messageText }/></td>
+              <td><Button text={ buttonText } onClick={ this.toggleLove }/></td>
+            </tr>
+          </tbody>
+        </table>
+      );
+    }
+  }
+);
+
+
+module.exports = Relationship;
